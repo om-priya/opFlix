7949f3a122349cbb0efeead5abae4c8a24996200
om priya <ompriya18153789@gmail.com>
2023-05-11 23:47:26 +0530

    Basic Navbar Done hamburger remains

---
+++ app/components/Ham.js
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { GiCrossMark } from "react-icons/gi";
+import { useState } from "react";
+
+export default function Ham() {
+  const [ham, setHam] = useState(false);
+  return (
+    <div
+      className="md:hidden mr-8 text-xl cursor-pointer"
+      onClick={() => setHam(!ham)}
+    >
+      {ham ? <GiCrossMark /> : <FaBars />}
+    </div>
+  );
+}
